b03ce5afe4f496561ec5bb6c360566a6167e4820
Khanh Le <khanhle.12cl@gmail.com>
2026-07-08 21:25:42 +0700

    update

--- lib/i18n/index.d.ts
+++ lib/i18n/index.d.ts
@@ -46,4 +46,3 @@ export { zhTW } from './locales/zh-TW';
 export { enUS } from './locales/en-US';
 export { viVN } from './locales/vi-VN';
 export * from './formatters';
-export { translateChart } from './translateChart';
